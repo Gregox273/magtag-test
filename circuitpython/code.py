5f09564a00e7497e18083dc577b13b86c456d237
Gregory Brooks <greg.brooks@signaloid.com>
2022-01-03 15:23:10 +0000

    Use buttons to change mode

--- circuitpython/code.py
+++ circuitpython/code.py
@@ -4,16 +4,19 @@ Gregory Brooks 2021
 
 # from adafruit_magtag.magtag import MagTag
 import alarm
+import board
+import time
 
 from octopusenergy import OctopusEnergy
 from openweather import OpenWeather
+
+PERIOD_MINUTES = 15
 
 def run(instance):
     instance.network.connect()
 
     try:
         instance.fetch()
-        instance.exit_and_deep_sleep(60 * 30)
     except (ValueError, RuntimeError) as e:
         print(e)
 
@@ -25,10 +28,22 @@ def run_octopus_energy():
     octopus_energy = OctopusEnergy()
     run(octopus_energy)
 
-if alarm.sleep_memory[0] > 0:
-    alarm.sleep_memory[0] = 0
-    run_open_weather_map()
-else:
-    alarm.sleep_memory[0] = 1
-    run_octopus_energy()
+def main():
+    wake_alarm = alarm.wake_alarm
+    buttons = (board.BUTTON_A, board.BUTTON_B)
+    pin_alarms = [alarm.pin.PinAlarm(pin=pin, value=False, pull=True)
+        for pin in buttons]
 
+    if hasattr(wake_alarm, "pin"):
+        if wake_alarm.pin == board.BUTTON_B:
+            run_octopus_energy()
+        else:
+            run_open_weather_map()
+    else:
+        run_open_weather_map()
+
+    time_alarm = alarm.time.TimeAlarm(
+        monotonic_time=int(time.monotonic()) + (60 * PERIOD_MINUTES))
+    alarm.exit_and_deep_sleep_until_alarms(time_alarm, *pin_alarms)
+
+main()
